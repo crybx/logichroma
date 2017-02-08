7b220ce7add3c44661841751c08710663ad8f89f
Crystal <git@cryb.org>
2017-02-08 09:13:47 -0500

    Adding initial code

---
+++ Pyrotechnics/Models/GameOptionsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pyrotechnics.Models
+{
+    public class GameOptionsModel
+    {
+        public int PlayerCount { get; set; }
+    }
+}
