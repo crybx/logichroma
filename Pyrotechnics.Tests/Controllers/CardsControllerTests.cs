110295cb411e58a6bfa2fb9eaca90def4e5cb788
crybx <crybx@users.noreply.github.com>
2017-02-23 10:06:55 -0500

    Added Moq to use for unit tests. Made enough other changes to test Moq in a single unit test.

---
+++ Pyrotechnics.Tests/Controllers/CardsControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Pyrotechnics.Controllers;
+using Pyrotechnics.Models.DataRepositoryInterfaces;
+
+namespace Pyrotechnics.Tests.Controllers
+{
+    [TestClass]
+    public class CardsControllerTests
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var cardRepo = new Mock<ICardRepository>();
+            var controller = new CardsController(cardRepo.Object);
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
