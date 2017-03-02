5fd11bca36691caf5c9d658d3a9f11f7bd7c67f5
Crystal <git@cryb.org>
2017-03-02 08:45:37 -0500

    Added basic ability to start a new game.

--- Pyrotechnics/Controllers/GameController.cs
+++ Pyrotechnics/Controllers/GameController.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Pyrotechnics.Models;
+using Pyrotechnics.Models.DataRepositories;
+using Pyrotechnics.Models.DataRepositoryInterfaces;
 
 namespace Pyrotechnics.Controllers
 {
     public class GameController : Controller
     {
-        public ActionResult StartNew()
+        private readonly IGameRepository _gameRepo;
+
+        public GameController(IGameRepository gameRepo)
         {
+            _gameRepo = gameRepo;
+        }
+
+        public GameController() : this(new GameRepository()) {}
+
+        public ActionResult Start()
+        {
+            var model = new GameOptionsModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Start(GameOptionsModel options)
+        {
+            var gameTitleAvaible = _gameRepo.IsGameNameAvailable(options.GameTitle);
+
+            if (gameTitleAvaible)
+            {
+                options.GameId = _gameRepo.AddGame(options);
+                return View("Index", options);
+            }
+
             return View();
         }
     }
