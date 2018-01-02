524873d6025d2c9e365e659bb56380ad0d0b517d
Crystal <git@cryb.org>
2018-01-01 22:28:19 -0500

    Creating the shuffled deck of cards when a new game is created.

--- Logichroma/Models/DataRepositories/GameRepository.cs
+++ Logichroma/Models/DataRepositories/GameRepository.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Linq;
+﻿using Logichroma.Extensions;
 using Logichroma.Models.Database;
 using Logichroma.Models.DataRepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Logichroma.Models.DataRepositories
 {
@@ -21,19 +23,67 @@ namespace Logichroma.Models.DataRepositories
             {
                 Name = gameOptions.GameTitle,
                 DifficultyLvl = 1,
-                NextCard = 1,
+                NextCard = 0,
                 StartDateTime = DateTime.Now
             };
 
             _db.Games.Add(game);
             _db.SaveChanges();
 
+            createShuffledDeck(game);
+
+            //TODO: create and save a game status of 'Created'
+
             return game.Id;
         }
 
-        public void CreateShuffledDeck()
+        private void createShuffledDeck(Game game)
         {
+            // Create the deck.
+            var deck = createDeck();
+
+            // Shuffle the deck.
+            deck.Shuffle();
+
+            // Record the order of the GameCard objects in the deck.
+            for (var i = 0; i < deck.Count; i++)
+            {
+                deck[i].Game = game;
+                deck[i].Order = i;
+            }
+
+            // Save the deck in the database.
+            _db.GameCards.AddRange(deck);
+            _db.SaveChanges();
+        }
+
+        private List<GameCard> createDeck()
+        {
+            var deck = new List<GameCard>();
+
+            var colors = _db.Colors.ToList();
+            var cardValues = _db.CardTypes.ToList();
+            var cardStateInDeck = _db.CardStates.FirstOrDefault(x => x.Name == "Deck");
             
+            foreach (var color in colors)
+            {
+                foreach (var cardValue in cardValues)
+                {
+                    for (var i = 0; i < cardValue.CountInDeck; i++)
+                    {
+                        var card = new GameCard
+                        {
+                            CardType = cardValue,
+                            Color = color,
+                            CardState = cardStateInDeck
+                        };
+
+                        deck.Add(card);
+                    }
+                }
+            }
+
+            return deck;
         }
     }
 }
