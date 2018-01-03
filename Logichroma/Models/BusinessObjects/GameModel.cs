0ce4d10d223b59a21d3cfc9a399628cc64a7199c
crybx <crybx@users.noreply.github.com>
2018-01-02 19:37:19 -0500

    Updated nuget packages.

---
+++ Logichroma/Models/BusinessObjects/GameModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Logichroma.Models.BusinessObjects
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public int PlayerCount { get; set; }
+        public int DifficultyLevel { get; set; }
+        public string GameTitle { get; set; }
+    }
+}
