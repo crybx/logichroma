dcc124404efdc1efcb7169ad343226a4a80c0732
crybx <crybx@users.noreply.github.com>
2018-01-08 13:45:59 -0500

    Added column to db for per game player nickname.

--- Logichroma.Database/dbo/Tables/GamePlayers.sql
+++ Logichroma.Database/dbo/Tables/GamePlayers.sql
@@ -3,7 +3,8 @@
 	[GameId] INT NOT NULL,
 	[PlayerId] NVARCHAR(128) NOT NULL,
 	[PlayerNumber] INT NULL,
-	PRIMARY KEY ([GameId], [PlayerId]),
+	[Nickname] NVARCHAR(50) NULL, 
+    PRIMARY KEY ([GameId], [PlayerId]),
 	CONSTRAINT [FK_GamePlayers_GameId] FOREIGN KEY (GameId) REFERENCES [Games]([Id]),
 	CONSTRAINT [FK_GamePlayers_PlayerId] FOREIGN KEY (PlayerId) REFERENCES [AspNetUsers]([Id])
 )
