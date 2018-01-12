cd2a121b3e23f826737ad8fb07f56d86d1fc6339
Crystal <git@cryb.org>
2018-01-11 21:19:38 -0500

    Updated database tables to be able to track cards in player hands.

--- Logichroma.Database/dbo/Tables/GameCards.sql
+++ Logichroma.Database/dbo/Tables/GameCards.sql
@@ -5,9 +5,11 @@
     [CardTypeId] INT NOT NULL,
     [CardStateId] INT NOT NULL,
     [ColorId] INT NOT NULL,
+	[GamePlayerId] INT NULL,
     PRIMARY KEY ([GameId], [Order]),
 	CONSTRAINT [FK_GameCards_GameId] FOREIGN KEY (GameId) REFERENCES [Games]([Id]),
 	CONSTRAINT [FK_GameCards_CardTypeId] FOREIGN KEY (CardTypeId) REFERENCES [CardTypes]([Id]),
 	CONSTRAINT [FK_GameCards_CardStateId] FOREIGN KEY ([CardStateId]) REFERENCES [CardStates]([Id]),
-	CONSTRAINT [FK_GameCards_ColorId] FOREIGN KEY (ColorId) REFERENCES [Colors]([Id])
+	CONSTRAINT [FK_GameCards_ColorId] FOREIGN KEY (ColorId) REFERENCES [Colors]([Id]),
+	CONSTRAINT [FK_GameCards_GamePlayerId] FOREIGN KEY (GamePlayerId) REFERENCES [GamePlayers]([GamePlayerId])
 )
--- Logichroma.Database/dbo/Tables/GamePlayers.sql
+++ Logichroma.Database/dbo/Tables/GamePlayers.sql
@@ -1,10 +1,11 @@
 ﻿CREATE TABLE [dbo].[GamePlayers]
 (
+	[GamePlayerId] INT IDENTITY NOT NULL PRIMARY KEY,
 	[GameId] INT NOT NULL,
 	[PlayerId] NVARCHAR(128) NOT NULL,
 	[PlayerNumber] INT NULL,
 	[Nickname] NVARCHAR(50) NULL, 
-    PRIMARY KEY ([GameId], [PlayerId]),
+    CONSTRAINT [UK_GamePlayers_GameId_PlayerId] UNIQUE ([GameId], [PlayerId]),
 	CONSTRAINT [FK_GamePlayers_GameId] FOREIGN KEY (GameId) REFERENCES [Games]([Id]),
 	CONSTRAINT [FK_GamePlayers_PlayerId] FOREIGN KEY (PlayerId) REFERENCES [AspNetUsers]([Id])
 )
