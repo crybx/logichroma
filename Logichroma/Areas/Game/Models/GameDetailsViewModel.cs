cf1bcb1f6b01da43fe1cd692346b370fa07c0118
Crystal <git@cryb.org>
2018-01-20 12:22:40 -0500

    Hide players own card details and display current player.

--- Logichroma/Areas/Game/Models/GameDetailsViewModel.cs
+++ Logichroma/Areas/Game/Models/GameDetailsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Logichroma.Areas.Game.Models.GameModels;
+using Logichroma.GameEngine.Enums;
 using System.Linq;
 
 namespace Logichroma.Areas.Game.Models
@@ -6,21 +7,31 @@ namespace Logichroma.Areas.Game.Models
     public class GameDetailsViewModel
     {
         public GameModel Game { get; set; }
+
         public string CurrentUserId { get; set; }
+
         public string PlayerNickname { get; set; }
-        public PlayerModel CurrentPlayer => Game?.GamePlayers?.FirstOrDefault(x => x.PlayerId == CurrentUserId);
+
+        public PlayerModel Player => Game?.GamePlayers?.FirstOrDefault(x => x.PlayerId == CurrentUserId);
+
+        public PlayerModel CurrentPlayer => Game?.PlayersInOrder?[Game.CurrentPlayerNumber];
+
+        public bool IsCurrentPlayer => Player == CurrentPlayer;
+
         public int PlayerCount => Game?.GamePlayers?.Count ?? 0;
 
+        public int DeckCount => Game?.GameCards?.Count(x => x.CardState == CardState.Deck.ToString()) ?? 0;
+
         public bool CanJoinGame => Game.Status == "Created"
-                                   && CurrentPlayer == null
+                                   && Player == null
                                    && PlayerCount < 5;
 
         public bool CanStartGame => Game.Status == "Created"
-                                    && CurrentPlayer != null
-                                    && CurrentPlayer.IsGameOwner
+                                    && Player != null
+                                    && Player.IsGameOwner
                                     && PlayerCount > 1;
 
         public bool CanPlayGame => Game.Status == "Started"
-                                   && CurrentPlayer != null;
+                                   && Player != null;
     }
 }
